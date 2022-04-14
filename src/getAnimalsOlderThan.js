e70cde3169e714f8c024efe3919fa2af0dd38059
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-14 19:07:08 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+// const leao = species.name;
+// const idadeDeZenna = species.residents.age;
+// console.log(leao);
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const getSpicie = species.find((specie) => specie.name === animal);
+  const getOlder = getSpicie.residents.every((bixinho) => bixinho.age > age);// se todos bixin tem a idade;
+  return getOlder;
 }
 
+// console.log(getAnimalsOlderThan('lions', 1));
 module.exports = getAnimalsOlderThan;
