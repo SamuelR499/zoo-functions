e60a5537f27149fd92797b3007f90e6f71e8c467
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-14 17:15:53 -0300

    requisito 1 ja foi tava facil ;)

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+// const animais = data.species
+const { species } = data
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) return [];
+
+  const contem = ids.map((id) => species.find((animal) => animal.id === id))
+  return contem;
 }
 
+console.log(getSpeciesByIds());
+
 module.exports = getSpeciesByIds;
+
+
+// function teste (...args){
+//   const escolhido =  args.filter( id => {
+//   if( id === 'id3'){
+//     return id
+//   }    
+
+//   });
+//   return escolhido[0]
+// }
+
+// console.log(teste('id1','id2','id3','id4'));
