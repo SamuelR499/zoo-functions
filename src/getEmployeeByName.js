d874f64d0bd1096a76abc8800b097a493870f8d0
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-15 13:21:24 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+// console.log(employees);
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const findEmploye = employees.find((trabaiador) => {
+    if (trabaiador.firstName === employeeName || trabaiador.lastName === employeeName) {
+      return trabaiador;
+    }
+    return false;
+  });
+  return findEmploye;
 }
+
+console.log(getEmployeeByName('e'));
 
 module.exports = getEmployeeByName;
