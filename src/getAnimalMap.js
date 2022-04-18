f5fa1bfe627ebff198cd17e6193dd30d8ddc3e30
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-18 16:24:17 -0300

    requisito 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+// console.log(species[0].name);
+const vazioFemaleTrue = species.reduce((acc, specie) => {
+  if (Object.keys(acc).includes(specie.location)) {
+    acc[specie.location].push(specie.name);
+  } else {
+    acc[specie.location] = [].push(specie.name);
+  }
+  return acc;
+}, { NE: [], NW: [], SE: [], SW: [] });
+// const includesName =
 
 function getAnimalMap(options) {
   // seu código aqui
-}
+  if (!options || options.sex === 'female' || options.sorted === true) {
+    return vazioFemaleTrue;
+  }
+  // if (options.includeNames === true) {
 
+  // }
+}
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+// console.log(species[0].residents);
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-}
+  const cuidaDe = employees.find((employee) => id === employee.id);
 
+  const especiesCuidadas = cuidaDe.responsibleFor.map((animalID) => species.filter(
+    (specie) => specie.id === animalID,
+  ));
+  const animaisCuidados = especiesCuidadas[0][0].residents;
+  const maisVelho = animaisCuidados.reduce((acc, animal) => (acc.age > animal.age ? acc : animal));
+  const result = [];
+  const { name, sex, age } = maisVelho;
+  result.push(name, sex, age);
+  return result;
+}
+console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = getOldestFromFirstSpecies;
