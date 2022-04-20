aef447a0f0431d686e8dda57c799dfc385f9d3a2
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-20 12:35:02 -0300

    requisito 10 ;)

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { employees, species } = data;
+
+const foundSpecie = (id) => species.find((specie) => specie.id === id);
+
+const array = [];
+const allEmployees = () => {
+  employees.forEach(({ id, firstName, lastName, responsibleFor: idAnimal }) =>
+    array.push({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: idAnimal.map((animal) => foundSpecie(animal).name),
+      locations: idAnimal.map((animal) => foundSpecie(animal).location),
+    }));
+  return array;
+};
+// console.log(allEmployees());
+
+function getEmployeesCoverage(obj) {
   // seu código aqui
+  if (!obj) return allEmployees();
+  const xablau = employees
+    .find((employee) =>
+      employee.firstName === obj.name || employee.lastName === obj.name || employee.id === obj.id);
+  if (!xablau) { throw new Error('Informações inválidas'); }
+  const { id, firstName, lastName, responsibleFor: idAnimal } = xablau;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: idAnimal.map((animal) => foundSpecie(animal).name),
+    locations: idAnimal.map((animal) => foundSpecie(animal).location),
+  };
 }
 
+// console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
